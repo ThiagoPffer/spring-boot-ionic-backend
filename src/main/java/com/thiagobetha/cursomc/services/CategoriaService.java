f311eeb2072355f4b71c6643a5aed8ec098a917b
Thiago Piffer <thiago.ramos@betha.com.br>
2021-05-25 08:42:08 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/thiagobetha/cursomc/services/CategoriaService.java
+++ src/main/java/com/thiagobetha/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.thiagobetha.cursomc.domain.Categoria;
 import com.thiagobetha.cursomc.repositories.CategoriaRepository;
+import com.thiagobetha.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +15,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria buscar(Integer id){
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-
+	
 }
