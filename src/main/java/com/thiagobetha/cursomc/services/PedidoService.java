5d16bc9a73ce4958cbb39a7cf9c3f9e814346e59
Thiago Piffer <thiago.ramos@betha.com.br>
2021-06-02 10:04:39 -0300

    Implementando toString de Pedido

--- src/main/java/com/thiagobetha/cursomc/services/PedidoService.java
+++ src/main/java/com/thiagobetha/cursomc/services/PedidoService.java
@@ -17,6 +17,7 @@ import com.thiagobetha.cursomc.repositories.PagamentoRepository;
 import com.thiagobetha.cursomc.repositories.PedidoRepository;
 import com.thiagobetha.cursomc.services.exceptions.ObjectNotFoundException;
 
+
 @Service
 public class PedidoService {
 
@@ -35,6 +36,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 	public Pedido find(Integer id){
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -45,6 +49,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		
@@ -57,11 +62,14 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for(ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		
 		itemPedidoRepository.saveAll(obj.getItens());
+		
+		System.out.println(obj);
 		return obj;
 	}
 	
