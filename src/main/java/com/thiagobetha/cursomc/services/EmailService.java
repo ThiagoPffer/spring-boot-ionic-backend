b3f9d2dbc1c580516f18e7493bd479f9a603082f
Thiago Piffer <thiago.ramos@betha.com.br>
2021-06-16 10:52:27 -0300

    Esqueci a senha

--- src/main/java/com/thiagobetha/cursomc/services/EmailService.java
+++ src/main/java/com/thiagobetha/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.thiagobetha.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.thiagobetha.cursomc.domain.Cliente;
 import com.thiagobetha.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,5 +10,7 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 	
 }
